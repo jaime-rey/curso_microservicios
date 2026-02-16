8e3a692a3ab2efa4e767c60d4ff18cd3c0203860
jaime rey <jaimereycasado@gmail.com>
2026-02-16 12:17:21 +0100

    Initial commit

---
+++ loans/src/main/java/dev/jaimerey/loans/LoansApplication.java
@@ -0,0 +1,34 @@
+package dev.jaimerey.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Accounts microservice REST API Documentation",
+                version = "v1.0",
+                description = "EazyBank Accounts microservice REST API documentation",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Jaime Rey",
+                        email = "jaimereycasado@gmail.com"
+                ),
+                license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "MIT License",
+                        url = "https://opensource.org/license/mit/"
+                )
+
+        )
+
+)
+public class LoansApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
